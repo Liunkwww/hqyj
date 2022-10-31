f5bc8b63b70956137e6e36b0d6ba9ff82247bde1
Poera <l2321230429@gmail.com>
2022-10-31 18:48:12 +0800

    first_commit

---
+++ day1/day1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(int argc, const char* argv[])
+{
+    int num = 0;
+    char a1[32];
+    printf("输入三位数的整数:");
+lable:
+    scanf("%d", &num);
+    if (num > 99 && num <= 999)
+    {
+        printf("百位:%d 十位:%d 个位:%d \n", num / 100, (num % 100) / 10, num % 10);
+    }
+    else
+    {
+        printf("输入不符合标准,请重新输入:");
+        gets(a1);
+        goto lable;
+    }
+    return 0;
+}
